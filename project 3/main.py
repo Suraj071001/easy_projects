252d228a21064fe893469b28b357725b7202b07e
Suraj <87514ss@gmail.com>
2022-03-02 21:40:29 +0530

    add more functions

--- project 3/main.py
+++ project 3/main.py
@@ -1,15 +1,24 @@
-class Grandfather() :
+class Names() :
     def __init__(self,*letter):
         self.letter = letter
         self.names = []
 
     def add(self):
         while True :
-            choice = int(input("Enter '1' if you wanna add name otherwise enter 'any number'"))
+            choice = int(input("\nEnter '1' for add name\n'2' for add list of names\n'any number' for exit :"))
             if choice == 1 :
                 name = input("Enter name :")
                 self.names.append(name)
-            elif choice != 1 :
+            elif choice == 2 :
+                name_list = []
+                no_of_name = int(input("Enter how many name you want in list : "))
+                while  no_of_name>0 :
+                    no_of_name -= 1
+                    name = input("Enter name : ")
+                    name_list.append(name)
+                self.names.append(name_list)
+
+            elif choice != 1 and 2 :
                 break
             else :
                 print("Check your input")
@@ -20,8 +29,12 @@ class Grandfather() :
         for name in self.names :
             if find in name :
                 print(name)
+            for n in name :
+                if find in n :
+                    print(n)
 
 
-gf = Grandfather()
+
+gf = Names()
 gf.add()
 gf.find()
